97e1e60f11293539ffec0c5c93c7e7bba6000afc
Gécen D. De Marchi <gecen.marchi@thread.com.br>
2018-09-14 15:05:42 -0300

    Migrando para maven

--- srcm/org/simalator/SimulatorExeption.java
+++
@@ -1,9 +0,0 @@
-package org.simalator;
-
-public class SimulatorExeption extends Exception{
-	private static final long serialVersionUID = 4010094460229295289L;
-
-	public SimulatorExeption(String msg){
-		super(msg);
-	}
-}
--- srcm/org/simalator/command/CommandInstruction.java
+++
@@ -1,7 +0,0 @@
-package org.simalator.command;
-
-import org.simalator.Simulator;
-
-public interface CommandInstruction {
-	public void execute (Simulator sim);
-}
